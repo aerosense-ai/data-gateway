924f01b5735014cda7e931715ca7adef7e0065e5
Tom Clark <tom@octue.com>
2021-01-27 16:57:44 +0000

    WIP Framework for simulated serial port

---
+++ tests/test_simulated_serial.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestSimulatedSerial(unittest.TestCase):
+    """Tests for the SimulatedSerial class"""
+
+    def test_a_thing(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
