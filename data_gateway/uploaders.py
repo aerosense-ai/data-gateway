3eb3ec52a588f5e8af1cd03266ac74d9ba1aff1e
cortadocodes <cortado.codes@protonmail.com>
2021-02-10 20:10:57 +0000

    CLN: Remove unused uploader

--- data_gateway/uploaders.py
+++ data_gateway/uploaders.py
@@ -1,6 +1,5 @@
 import logging
 import time
-import pkg_resources
 from octue.utils.cloud.persistence import GoogleCloudStorageClient
 
 
@@ -8,38 +7,6 @@ logger = logging.getLogger(__name__)
 
 
 CLOUD_DIRECTORY_NAME = "data_gateway"
-
-
-class Uploader:
-    """Handler class for HTTPS-based uploads of events and audio files"""
-
-    def __init__(self, configuration=None, **kwargs):
-        """Instantiate and configure gateway server"""
-        self.streams = {sensor_type: [] for sensor_type in configuration["sensor_types"]}
-
-    def add_to_stream(self, sensor_type, data):
-        self.streams[sensor_type].append(data)
-
-    def upload(self, project_name, bucket_name, directory_in_bucket, extension):
-        client = GoogleCloudStorageClient(project_name=project_name)
-
-        for stream_name, data in self.streams.items():
-            client.upload_from_string(
-                serialised_data="".join(data),
-                bucket_name=bucket_name,
-                path_in_bucket=f"{directory_in_bucket}/{stream_name}{extension}",
-            )
-            self.streams[stream_name] = []
-
-    def _validate_event(self):
-        """Validate event data against the required schema"""
-        file_name = pkg_resources.resource_string("data_gateway", "schema/event_schema.json")
-        logger.info("file name is %s", file_name)
-
-    def _validate_audio(self):
-        """Validate audio+meta data against the required schema"""
-        file_name = pkg_resources.resource_string("data_gateway", "schema/audio_meta_schema.json")
-        logger.info("file name is %s", file_name)
 
 
 class StreamingUploader:
