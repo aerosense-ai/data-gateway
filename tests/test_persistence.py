730cf7a56302fc54764845cbb44167b31e8ed54f
cortadocodes <cortado.codes@protonmail.com>
2021-02-15 16:13:06 +0000

    REF: Rename test module; swap test class order

--- tests/test_persistence.py
+++ tests/test_persistence.py
@@ -10,6 +10,55 @@ from octue.utils.cloud.persistence import GoogleCloudStorageClient
 from data_gateway.persistence import BATCH_DIRECTORY_NAME, BatchingFileWriter, BatchingUploader
 
 
+class TestBatchingWriter(unittest.TestCase):
+    def test_data_is_batched(self):
+        """Test that data is added to the correct stream as expected."""
+        with tempfile.TemporaryDirectory() as temporary_directory:
+            writer = BatchingFileWriter(
+                sensor_specifications=[{"name": "test", "extension": ".csv"}],
+                directory_path=temporary_directory,
+                batch_interval=600,
+            )
+
+        stream = writer.current_batches["test"]
+        self.assertEqual(stream["name"], "test"),
+        self.assertEqual(stream["data"], [])
+        self.assertEqual(stream["batch_number"], 0),
+        self.assertEqual(stream["extension"], ".csv")
+
+        writer.add_to_current_batch(sensor_name="test", data="blah,")
+        self.assertEqual(writer.current_batches["test"]["data"], ["blah,"])
+
+    def test_data_is_written_to_disk_in_batches(self):
+        """Test that data is written to disk in batches of whatever units it is added to the stream in."""
+        with tempfile.TemporaryDirectory() as temporary_directory:
+            writer = BatchingFileWriter(
+                sensor_specifications=[{"name": "test", "extension": ".csv"}],
+                directory_path=temporary_directory,
+                batch_interval=0.01,
+            )
+
+            with writer:
+                writer.add_to_current_batch(sensor_name="test", data="ping,")
+                writer.add_to_current_batch(sensor_name="test", data="pong,\n")
+                self.assertEqual(len(writer.current_batches["test"]["data"]), 2)
+                time.sleep(0.01)
+
+                writer.add_to_current_batch(sensor_name="test", data="ding,")
+                self.assertEqual(len(writer.current_batches["test"]["data"]), 0)
+
+                writer.add_to_current_batch(sensor_name="test", data="dong,\n")
+                time.sleep(0.01)
+
+            self.assertEqual(len(writer.current_batches["test"]["data"]), 0)
+
+            with open(os.path.join(temporary_directory, BATCH_DIRECTORY_NAME, "test", "batch-0.csv")) as f:
+                self.assertEqual(f.read(), "ping,pong,\nding,")
+
+            with open(os.path.join(temporary_directory, BATCH_DIRECTORY_NAME, "test", "batch-1.csv")) as f:
+                self.assertEqual(f.read(), "dong,\n")
+
+
 class TestBatchingUploader(unittest.TestCase):
 
     TEST_PROJECT_NAME = os.environ["TEST_PROJECT_NAME"]
@@ -85,52 +134,3 @@ class TestBatchingUploader(unittest.TestCase):
             ),
             "dong,\n",
         )
-
-
-class TestBatchingWriter(unittest.TestCase):
-    def test_data_is_batched(self):
-        """Test that data is added to the correct stream as expected."""
-        with tempfile.TemporaryDirectory() as temporary_directory:
-            writer = BatchingFileWriter(
-                sensor_specifications=[{"name": "test", "extension": ".csv"}],
-                directory_path=temporary_directory,
-                batch_interval=600,
-            )
-
-        stream = writer.current_batches["test"]
-        self.assertEqual(stream["name"], "test"),
-        self.assertEqual(stream["data"], [])
-        self.assertEqual(stream["batch_number"], 0),
-        self.assertEqual(stream["extension"], ".csv")
-
-        writer.add_to_current_batch(sensor_name="test", data="blah,")
-        self.assertEqual(writer.current_batches["test"]["data"], ["blah,"])
-
-    def test_data_is_written_to_disk_in_batches(self):
-        """Test that data is written to disk in batches of whatever units it is added to the stream in."""
-        with tempfile.TemporaryDirectory() as temporary_directory:
-            writer = BatchingFileWriter(
-                sensor_specifications=[{"name": "test", "extension": ".csv"}],
-                directory_path=temporary_directory,
-                batch_interval=0.01,
-            )
-
-            with writer:
-                writer.add_to_current_batch(sensor_name="test", data="ping,")
-                writer.add_to_current_batch(sensor_name="test", data="pong,\n")
-                self.assertEqual(len(writer.current_batches["test"]["data"]), 2)
-                time.sleep(0.01)
-
-                writer.add_to_current_batch(sensor_name="test", data="ding,")
-                self.assertEqual(len(writer.current_batches["test"]["data"]), 0)
-
-                writer.add_to_current_batch(sensor_name="test", data="dong,\n")
-                time.sleep(0.01)
-
-            self.assertEqual(len(writer.current_batches["test"]["data"]), 0)
-
-            with open(os.path.join(temporary_directory, BATCH_DIRECTORY_NAME, "test", "batch-0.csv")) as f:
-                self.assertEqual(f.read(), "ping,pong,\nding,")
-
-            with open(os.path.join(temporary_directory, BATCH_DIRECTORY_NAME, "test", "batch-1.csv")) as f:
-                self.assertEqual(f.read(), "dong,\n")
