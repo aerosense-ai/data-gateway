cdf3b9d81f16dd5fa2803c3b1a44f29a37a61a3b
cortadocodes <cortado.codes@protonmail.com>
2021-02-16 16:03:27 +0000

    TST: Test providing a config file to CLI

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+import os
 from unittest import TestCase, mock
 from click.testing import CliRunner
 
@@ -19,10 +20,18 @@ class TestCLI(TestCase):
         h_result = CliRunner().invoke(gateway_cli, ["-h"])
         assert help_result.output == h_result.output
 
-    def test_start_and_stop(self):
+    def test_start_and_stop_in_interactive_mode(self):
         """Ensure the gateway can be started and stopped via the CLI in interactive mode."""
         with mock.patch("serial.Serial", new=DummySerial):
             result = CliRunner().invoke(gateway_cli, "start --interactive", input="stop\n")
             self.assertIsNone(result.exception)
             self.assertEqual(result.exit_code, 0)
             self.assertTrue("Stopping gateway." in result.output)
+
+    def test_start_with_config_file(self):
+        """Ensure a configuration file can be provided via the CLI."""
+        config_path = os.path.join(os.path.dirname(__file__), "valid_configuration.json")
+        with mock.patch("serial.Serial", new=DummySerial):
+            result = CliRunner().invoke(gateway_cli, f"start --interactive --config-file={config_path}", input="stop\n")
+
+        self.assertTrue("Loaded configuration file" in result.output)
