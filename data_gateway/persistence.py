a22a13fe6ab218e18b3b114364230737c1df945d
cortadocodes <cortado.codes@protonmail.com>
2021-02-17 19:31:08 +0000

    IMP: Add filter option to get_oldest_file_in_directory

--- data_gateway/persistence.py
+++ data_gateway/persistence.py
@@ -21,11 +21,11 @@ def calculate_disk_usage(path, filter=None):
     considered and False for those that shouldn't.
 
     :param str path:
-    :param callable filter:
+    :param callable|None filter:
     :return float:
     """
     if os.path.isfile(path):
-        if not filter:
+        if filter is None:
             return os.path.getsize(path)
 
         if filter(path):
@@ -35,13 +35,18 @@ def calculate_disk_usage(path, filter=None):
     return sum(calculate_disk_usage(item.path) for item in os.scandir(path))
 
 
-def get_oldest_file_in_directory(path):
-    """Get the oldest file in a directory. This is not a recursive function.
+def get_oldest_file_in_directory(path, filter=None):
+    """Get the oldest file in a directory. This is not a recursive function. The files considered can be filtered by a
+    callable that returns True for paths that should be considered and False for those that shouldn't.
 
     :param str path:
+    :param callable|None filter:
     :return str|None:
     """
-    contents = [item for item in os.scandir(path) if item.is_file()]
+    if filter is None:
+        contents = [item for item in os.scandir(path) if item.is_file()]
+    else:
+        contents = [item for item in os.scandir(path) if item.is_file() and filter(item.path)]
 
     try:
         return min(contents, key=os.path.getctime).path
