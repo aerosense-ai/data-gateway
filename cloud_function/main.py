3756220c9d3ca507695e3bb2f60c0ef6fb40d3d4
cortadocodes <cortado.codes@protonmail.com>
2021-02-19 17:29:02 +0000

    IMP: Add cloud function for cleaning and moving batches

---
+++ cloud_function/main.py
@@ -0,0 +1,40 @@
+import json
+import logging
+import os
+from octue.utils.cloud.persistence import GoogleCloudStorageClient
+
+
+logger = logging.getLogger(__name__)
+
+
+def clean_and_upload_batch(event, context):
+    """Triggered by a change to a Cloud Storage bucket.
+
+    :param dict event:
+    :param google.cloud.functions.Context context: metadata for the event
+    :return None
+    """
+    project_name = os.environ["PROJECT_NAME"]
+    destination_bucket = os.environ["DESTINATION_BUCKET"]
+
+    client = GoogleCloudStorageClient(project_name=project_name)
+
+    batch = json.loads(client.download_as_string(bucket_name=event["bucket"], path_in_bucket=event["name"]))
+    logger.debug("Received batch %r from bucket %r for cleaning.", event["name"], event["bucket"])
+
+    cleaned_batch = clean(batch, event)
+
+    client.upload_from_string(
+        serialised_data=json.dumps(cleaned_batch), bucket_name=destination_bucket, path_in_bucket=event["name"]
+    )
+    logger.info("Cleaned and uploaded cleaned batch %r to bucket %r.", event["name"], destination_bucket)
+
+
+def clean(batch, event):
+    """Clean and return the given batch.
+
+    :param dict batch:
+    :param dict event:
+    :return dict:
+    """
+    pass
---
+++ cloud_function/tests.py
@@ -0,0 +1,67 @@
+import json
+import os
+import unittest
+from unittest import mock
+from gcloud_storage_emulator.server import create_server
+from google.cloud import storage
+from octue.utils.cloud.credentials import GCPCredentialsManager
+from octue.utils.cloud.persistence import GoogleCloudStorageClient
+
+from cloud_function import main
+
+
+class TestCloudFunction(unittest.TestCase):
+
+    TEST_PROJECT_NAME = os.environ["TEST_PROJECT_NAME"]
+    TEST_BUCKET_NAME = os.environ["TEST_BUCKET_NAME"]
+    storage_emulator = create_server("localhost", 9090, in_memory=True)
+
+    @classmethod
+    def setUpClass(cls):
+        os.environ["PROJECT_NAME"] = cls.TEST_PROJECT_NAME
+        os.environ["DESTINATION_BUCKET"] = cls.TEST_BUCKET_NAME
+
+        cls.storage_emulator.start()
+
+        storage.Client(
+            project=cls.TEST_PROJECT_NAME, credentials=GCPCredentialsManager().get_credentials()
+        ).create_bucket(bucket_or_name=cls.TEST_BUCKET_NAME)
+
+        GoogleCloudStorageClient(cls.TEST_PROJECT_NAME).upload_from_string(
+            serialised_data=json.dumps({"Baros": "blah,blah,hello,\n"}),
+            bucket_name=cls.TEST_BUCKET_NAME,
+            path_in_bucket="batch-0.json",
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.storage_emulator.stop()
+
+    def test_clean_and_upload_batch(self):
+        """Test that a finalise event from a Google Cloud Storage bucket triggers cleaning of a batch file and its
+        upload to a destination bucket.
+        """
+        event = {
+            "bucket": self.TEST_BUCKET_NAME,
+            "name": "batch-0.json",
+            "metageneration": "some-metageneration",
+            "timeCreated": "0",
+            "updated": "0",
+        }
+
+        context = unittest.mock.MagicMock()
+        context.event_id = "some-id"
+        context.event_type = "gcs-event"
+
+        with mock.patch("cloud_function.main.clean", return_value={"baros": "hello,\n"}):
+            main.clean_and_upload_batch(event, context)
+
+        self.assertEqual(
+            json.loads(
+                GoogleCloudStorageClient(project_name=self.TEST_PROJECT_NAME).download_as_string(
+                    bucket_name=self.TEST_BUCKET_NAME,
+                    path_in_bucket="batch-0.json",
+                )
+            ),
+            {"baros": "hello,\n"},
+        )
