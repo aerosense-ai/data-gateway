e6ea4e40f2602130ba185af0f99679fd9bca1223
cortadocodes <cortado.codes@protonmail.com>
2021-02-19 18:54:02 +0000

    IMP: Delete uncleaned batch after clean batch upload

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -16,18 +16,22 @@ def clean_and_upload_batch(event, context):
     """
     destination_project_name = os.environ["DESTINATION_PROJECT_NAME"]
     destination_bucket = os.environ["DESTINATION_BUCKET"]
+    source_bucket = event["bucket"]
+    batch_path = event["name"]
 
     client = GoogleCloudStorageClient(project_name=destination_project_name, credentials=None)
 
-    batch = json.loads(client.download_as_string(bucket_name=event["bucket"], path_in_bucket=event["name"]))
-    logger.debug("Received batch %r from bucket %r for cleaning.", event["name"], event["bucket"])
+    batch = json.loads(client.download_as_string(bucket_name=source_bucket, path_in_bucket=batch_path))
+    logger.debug("Received batch %r from bucket %r for cleaning.", batch_path, source_bucket)
 
     cleaned_batch = clean(batch, event)
 
     client.upload_from_string(
-        serialised_data=json.dumps(cleaned_batch), bucket_name=destination_bucket, path_in_bucket=event["name"]
+        serialised_data=json.dumps(cleaned_batch), bucket_name=destination_bucket, path_in_bucket=batch_path
     )
-    logger.info("Cleaned and uploaded cleaned batch %r to bucket %r.", event["name"], destination_bucket)
+    logger.info("Cleaned and uploaded cleaned batch %r to bucket %r.", batch_path, destination_bucket)
+
+    client.delete(bucket_name=source_bucket, path_in_bucket=batch_path)
 
 
 def clean(batch, event):
