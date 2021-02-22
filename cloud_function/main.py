189917336f254e53d00c7629efcbbeb362a29011
cortadocodes <cortado.codes@protonmail.com>
2021-02-22 19:47:23 +0000

    REF: Factor out getting of batch in cloud function

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -18,19 +18,13 @@ def clean_and_upload_batch(event, context, cleaned_batch_name=None):
     :param google.cloud.functions.Context context: metadata for the event
     :return None
     """
-    source_bucket_name = event["bucket"]
-    batch_path = event["name"]
-    cleaned_batch_path = os.path.join(os.path.split(batch_path)[0], cleaned_batch_name) or batch_path
     destination_project_name = os.environ["DESTINATION_PROJECT_NAME"]
     destination_bucket_name = os.environ["DESTINATION_BUCKET"]
-
     client = GoogleCloudStorageClient(project_name=destination_project_name, credentials=None)
 
-    batch = json.loads(client.download_as_string(bucket_name=source_bucket_name, path_in_bucket=batch_path))
-    batch_metadata = client.get_metadata(bucket_name=source_bucket_name, path_in_bucket=batch_path)
-    logger.debug("Received batch %r from bucket %r for cleaning.", batch_path, source_bucket_name)
-
+    batch, batch_metadata, batch_path = get_batch(client, event)
     cleaned_batch = clean(batch, batch_metadata, event)
+    cleaned_batch_path = os.path.join(os.path.split(batch_path)[0], cleaned_batch_name) or batch_path
 
     client.upload_from_string(
         serialised_data=json.dumps(cleaned_batch),
@@ -53,7 +47,24 @@ def clean_and_upload_batch(event, context, cleaned_batch_name=None):
 
     logger.info("Cleaned and uploaded cleaned batch %r to bucket %r.", batch_path, destination_bucket_name)
 
-    client.delete(bucket_name=source_bucket_name, path_in_bucket=batch_path)
+    client.delete(bucket_name=event["bucket"], path_in_bucket=batch_path)
+
+
+def get_batch(storage_client, event):
+    """Get the batch from Google Cloud storage.
+
+    :param octue.utils.cloud.persistence.GoogleCloudStorageClient storage_client:
+    :param dict event:
+    :return (dict, dict, str):
+    """
+    source_bucket_name = event["bucket"]
+    batch_path = event["name"]
+
+    batch = json.loads(storage_client.download_as_string(bucket_name=source_bucket_name, path_in_bucket=batch_path))
+    batch_metadata = storage_client.get_metadata(bucket_name=source_bucket_name, path_in_bucket=batch_path)
+    logger.debug("Received batch %r from bucket %r for cleaning.", batch_path, source_bucket_name)
+
+    return batch, batch_metadata, batch_path
 
 
 def clean(batch, batch_metadata, event):
