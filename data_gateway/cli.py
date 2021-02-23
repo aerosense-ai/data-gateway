135a97e15b6896c6b6ba45adf16f551812297706
cortadocodes <cortado.codes@protonmail.com>
2021-02-23 12:07:09 +0000

    CLN: Clean up CLI

--- data_gateway/cli.py
+++ data_gateway/cli.py
@@ -16,8 +16,6 @@ SUPERVISORD_PROGRAM_NAME = "AerosenseGateway"
 
 logger = logging.getLogger(__name__)
 
-global_cli_context = {}
-
 
 @click.group(context_settings={"help_option_names": ["-h", "--help"]})
 @click.option(
@@ -25,20 +23,19 @@ global_cli_context = {}
     default=None,
     type=click.STRING,
     show_default=True,
-    help="Stream logs to a websocket at the given URI (useful for monitoring what's happening remotely)",
+    help="Stream logs to a websocket at the given URI (useful for monitoring what's happening remotely).",
 )
 @click.option(
     "--log-level",
     default="info",
     type=click.Choice(["debug", "info", "warning", "error"], case_sensitive=False),
     show_default=True,
-    help="Log level used for the analysis.",
+    help="Set the log level.",
 )
 @click.version_option(version=pkg_resources.get_distribution("data_gateway").version)
 def gateway_cli(logger_uri, log_level):
-    """AeroSense Gateway CLI.
-
-    Runs the on-nacelle gateway service to read data from the bluetooth receivers and send it to AeroSense Cloud.
+    """AeroSense Gateway CLI. Run the on-nacelle gateway service to read data from the bluetooth receivers and send it
+    to AeroSense Cloud.
     """
     if logger_uri:
         handler = get_remote_handler(logger_uri=logger_uri, log_level=log_level)
@@ -51,10 +48,10 @@ def gateway_cli(logger_uri, log_level):
 @gateway_cli.command()
 @click.option(
     "--config-file",
-    type=click.Path(),
+    type=click.Path(dir_okay=False),
     default="config.json",
     show_default=True,
-    help="Path to your aerosense deployment configuration file.",
+    help="Path to your Aerosense deployment configuration file.",
 )
 @click.option(
     "--interactive",
@@ -64,8 +61,8 @@ def gateway_cli(logger_uri, log_level):
     show_default=True,
     help="""
         Run the gateway in interactive mode, allowing commands to be sent to the serial port via the command line.\n
-        WARNING: the output of the gateway will be saved to files locally and not uploaded to the cloud if this
-        option is used. The same file/folder structure will be used either way.
+        WARNING: the output of the gateway will be saved to disk locally and not uploaded to the cloud if this option
+        is used. The same file/folder structure will be used either way.
     """,
 )
 @click.option(
@@ -73,7 +70,7 @@ def gateway_cli(logger_uri, log_level):
     type=click.Path(file_okay=False),
     default="data_gateway",
     show_default=True,
-    help="The directory in which to save the batch files.",
+    help="The directory in which to save batch files from the data gateway.",
 )
 @click.option(
     "--batch-interval",
@@ -145,25 +142,24 @@ def start(
         configuration=config,
     )
 
-    threading.Thread(target=packet_reader.read_packets, args=(serial_port, stop_when_no_more_data), daemon=True)
-    print(
-        "Starting gateway in interactive mode - files will *not* be uploaded to cloud storage but will instead be saved"
-        f" to disk at {os.path.join('.', packet_reader.writer.output_directory)!r} at intervals of {batch_interval} "
-        "seconds."
-    )
-
     if not output_dir.startswith("/"):
         output_dir = os.path.join(".", output_dir)
 
+    threading.Thread(target=packet_reader.read_packets, args=(serial_port, stop_when_no_more_data), daemon=True)
+    print(
+        "Starting gateway in interactive mode - files will *not* be uploaded to cloud storage but will instead be saved"
+        f" to disk at {output_dir!r} at intervals of {batch_interval} seconds."
+    )
+
+    # Keep a record of the commands given.
     commands_record_file = os.path.join(output_dir, "commands.txt")
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     try:
         while not packet_reader.stop:
             for line in sys.stdin:
-
-                # Keep a record of the commands given.
-                if not os.path.exists(output_dir):
-                    os.makedirs(output_dir)
 
                 with open(commands_record_file, "a") as f:
                     f.write(line)
@@ -187,13 +183,11 @@ def start(
     type=click.Path(),
     default="config.json",
     show_default=True,
-    help="Path to your aerosense deployment configuration file.",
+    help="Path to your Aerosense deployment configuration file.",
 )
 def supervisord_conf(config_file):
-    """Print conf entry for use with supervisord
-
-    Daemonising a process ensures it automatically restarts after a failure and on startup of the operating system
-    failure.
+    """Print conf entry for use with supervisord. Daemonising a process ensures it automatically restarts after a
+    failure and on startup of the operating system failure.
     """
     supervisord_conf_str = f"""
 
