fa8b46bd4b18bac4fe0165d0ecf550823614eabe
Juri <yuriy.marykovskiy@mail.polimi.it>
2021-07-04 15:31:26 +0200

    Data cleaning test

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,46 +1,77 @@
+import datetime
+import json
+import os
 import unittest
 import numpy as np
-import pandas as pd
-from scipy.stats import uniform
 
-from data_preprocess import MeasurementData, preprocess
+from data_preprocess import preprocess
 
 
 class TestDataPreProcess(unittest.TestCase):
-    def sample_dataframe(self, rows, cols):
 
-        data = uniform.rvs(size=rows * cols)
-        time = pd.date_range("1/1/2000", periods=rows, freq="0.01S")
+    path_to_sample_data = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)),
+        "tests",
+        ".test_data",
+        "AVENTA",
+        "21.06.2021",
+        "424536",
+        "window-0.json",
+    )
+    configuration_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "tests", "valid_configuration.json")
+    with open(configuration_path) as f:
+        VALID_CONFIGURATION = json.load(f)
 
-        test_df = pd.DataFrame(data.reshape(-1, cols), index=time)
-        return test_df
+    def sample_dataset(self):
+        with open(self.path_to_sample_data, "r") as in_file:
+            sample_data = json.load(in_file)
+        return sample_data
 
-    def sample_batch(self):
-        test_batch = {
-            "Mics": self.sample_dataframe(100, 10).to_csv(header=False),
-            "Baros_P": self.sample_dataframe(10, 40).to_csv(header=False),
-            "Baros_T": self.sample_dataframe(10, 40).to_csv(header=False),
-            "Acc": self.sample_dataframe(10, 3).to_csv(header=False),
-            "Gyro": self.sample_dataframe(10, 3).to_csv(header=False),
-            "Mag": self.sample_dataframe(10, 3).to_csv(header=False),
-            "Analog Vbat": self.sample_dataframe(10, 1).to_csv(header=False),
-            "Constat": self.sample_dataframe(10, 4).to_csv(header=False),
-        }
+    def random_dataset(self, rows, cols):
+        data = np.random.rand(rows, cols)
+        time = np.linspace(0, 10, rows)
+        random_data = np.append(np.transpose([time]), data, axis=1)
+        return random_data
+
+    def random_batch(self):
+        sensors = {"Mics"}
+        # sensors = {"Mics", "Baros_P", "Baros_T", "Acc", "Gyro", "Mag"}
+
+        test_batch = {"start_timestamp": datetime.datetime.now().timestamp(), "sensor_data": {}}
+
+        for sensor in sensors:
+            test_batch["sensor_data"][sensor] = self.random_dataset(
+                int(10 / self.VALID_CONFIGURATION["period"][sensor]),  # 10 seconds long
+                self.VALID_CONFIGURATION["n_meas_qty"][sensor],
+            )
 
         return test_batch
 
-    def test_outlier_detection(self):
-        """Tests parameters of outlier detection function definition"""
-        df = self.sample_dataframe(100, 1)
-        df.iloc[50, 0] = 0.5 + np.sqrt(1 / 12) * 1000  # Variance of uniform dist is 1/12*(a-b)^2
-        cleaned = MeasurementData.remove_outliers(df, 10, 3)
-        self.assertTrue(np.isnan(cleaned.iloc[50, 0]))
-
     def test_missing_data(self):
-        test_batch = self.sample_batch()
-        test_metadata = {}
+        test_batch = self.random_batch()
+        test_metadata = self.VALID_CONFIGURATION
+        # add some noise to timestamps
+        test_batch["sensor_data"]["Mics"][:, 0] += np.random.rand(test_batch["sensor_data"]["Mics"][:, 0].size) / 1e8
+        # remove some rows
+        test_batch["sensor_data"]["Mics"] = np.delete(test_batch["sensor_data"]["Mics"], slice(40, 61), 0)
 
-        preprocess.run(test_batch, test_metadata)
+        processed_batch = preprocess.run(test_batch, test_metadata)
 
-    def test_resample(self):
-        """Tests resampling function"""
+        # Check if the data got padded with NaN
+        self.assertTrue(np.isnan(processed_batch["Mics"][50][1]) and not np.isnan(processed_batch["Mics"][1][1]))
+
+    """
+    def test_sample_dataset(self):
+        test_batch = self.sample_dataset()
+        test_metadata = self.VALID_CONFIGURATION
+
+        # TODO What is vbat?
+        del test_batch['sensor_data']['Analog Vbat']
+        # TODO constat freqs should be in valid configuration
+        del test_batch['sensor_data']['Constat']
+
+        processed_batch = preprocess.run(test_batch, test_metadata)
+
+        with open('../../../apps/visualisers/time-series/data/out.json', 'w') as out_file:
+            json.dump(processed_batch, out_file)
+    """
