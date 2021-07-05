93ed0d7322dd340ccd776f8fae87a014c1efc888
Juri <yuriy.marykovskiy@mail.polimi.it>
2021-07-05 22:39:13 +0200

    TST: Fix the test

--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -7,10 +7,13 @@ from cloud_function.file_handler import FileHandler
 class TestFileHandler(unittest.TestCase):
     def test_clean(self):
         """Test clean function returns batch in expected format."""
-        batch = {"start_timestamp": datetime.datetime(2000, 1, 1).timestamp(), "sensor_data": {"Baros": [1, 2, 3, 4]}}
+        batch = {
+            "start_timestamp": datetime.datetime(2000, 1, 1).timestamp(),
+            "sensor_data": {"Baros_P": [[0, 1, 2, 3], [1, 1, 2, 3]]},
+        }
 
         cleaned_batch = FileHandler(
             source_project="test", source_bucket="test", destination_project="test", destination_bucket="test"
-        ).clean(batch, {}, {})
+        ).clean(batch, {"period": {"Baros_P": 1}}, {})
 
         self.assertTrue("cleaned" in cleaned_batch.keys())
