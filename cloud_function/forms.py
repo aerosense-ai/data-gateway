f2f38264fdff74a0a76c05047418599f0604fe6b
cortadocodes <cortado.codes@protonmail.com>
2021-11-09 19:27:40 +0000

    FIX: Make longitude/latitude optional in create-installation command

--- cloud_function/forms.py
+++ cloud_function/forms.py
@@ -5,5 +5,5 @@ from wtforms import FloatField, StringField, validators
 class CreateInstallationForm(FlaskForm):
     reference = StringField("Reference", [validators.DataRequired()])
     hardware_version = StringField("Hardware version", [validators.DataRequired()])
-    longitude = FloatField("Longitude", [validators.DataRequired()])
-    latitude = FloatField("Latitude", [validators.DataRequired()])
+    longitude = FloatField("Longitude", [validators.Optional()])
+    latitude = FloatField("Latitude", [validators.Optional()])
