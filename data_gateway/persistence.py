3400f74f319934804364f25fbf11acfa81eb9bc7
cortadocodes <cortado.codes@protonmail.com>
2022-02-02 19:08:35 +0000

    FIX: Add force_persist method to NoOperationContextManager

--- data_gateway/persistence.py
+++ data_gateway/persistence.py
@@ -30,6 +30,13 @@ class NoOperationContextManager:
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
 
+    def force_persist(self):
+        """Do nothing.
+
+        :return None:
+        """
+        pass
+
 
 class TimeBatcher:
     """A batcher that groups the given data into time windows.
