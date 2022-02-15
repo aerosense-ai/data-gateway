c264f7aeb44d9ac1ba0528f53555b0f639cff982
cortadocodes <cortado.codes@protonmail.com>
2022-02-15 15:53:15 +0000

    ENH: Stop the gateway if there's an error in a Routine execution

--- data_gateway/routine.py
+++ data_gateway/routine.py
@@ -41,37 +41,45 @@ class Routine:
                 logger.warning("The `stop_after` parameter is ignored unless `period` is also given.")
 
     def run(self, stop_signal):
-        """Send the commands to the action after the given delays, repeating if a period was given.
+        """Send the commands to the action after the given delays, repeating if a period was given. The routine will
+        stop before the next run if the stop signal is received (i.e. if the `stop_signal.value` is set to 1 in another
+        process).
 
+        :param multiprocessing.Value stop_signal: a value of 0 means don't stop; a value of 1 means stop
         :return None:
         """
-        scheduler = sched.scheduler(time.perf_counter)
-        start_time = time.perf_counter()
+        try:
+            scheduler = sched.scheduler(time.perf_counter)
+            start_time = time.perf_counter()
 
-        while stop_signal.value == 0:
-            cycle_start_time = time.perf_counter()
+            while stop_signal.value == 0:
+                cycle_start_time = time.perf_counter()
 
-            for command, delay in self.commands:
-                scheduler.enter(delay=delay, priority=1, action=self.action, argument=(command,))
+                for command, delay in self.commands:
+                    scheduler.enter(delay=delay, priority=1, action=self.action, argument=(command,))
 
-                # If a command is "stop", schedule stopping the gateway and then schedule no further commands.
-                if command == "stop":
-                    scheduler.enter(delay=delay, priority=1, action=stop_gateway, argument=(logger, stop_signal))
-                    break
+                    # If a command is "stop", schedule stopping the gateway and then schedule no further commands.
+                    if command == "stop":
+                        scheduler.enter(delay=delay, priority=1, action=stop_gateway, argument=(logger, stop_signal))
+                        break
 
-            scheduler.run(blocking=True)
+                scheduler.run(blocking=True)
 
-            if self.period is None:
-                logger.info("Non-periodic routine finished.")
-                return
-
-            elapsed_time = time.perf_counter() - cycle_start_time
-            time.sleep(self.period - elapsed_time)
-
-            if self.stop_after:
-                if time.perf_counter() - start_time >= self.stop_after:
-                    logger.info("Periodic routine stopped after given timeout of %ss.", self.stop_after)
+                if self.period is None:
+                    logger.info("Non-periodic routine finished.")
                     return
+
+                elapsed_time = time.perf_counter() - cycle_start_time
+                time.sleep(self.period - elapsed_time)
+
+                if self.stop_after:
+                    if time.perf_counter() - start_time >= self.stop_after:
+                        logger.info("Periodic routine stopped after given timeout of %ss.", self.stop_after)
+                        return
+
+        except Exception as e:
+            stop_gateway(logger, stop_signal)
+            raise e
 
     def _wrap_action_with_logger(self, action):
         """Wrap the given action so that when it's run on a command, the command is logged.
