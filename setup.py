843b0c3b8776f1f9095958fb345287374e14fa6f
cortadocodes <cortado.codes@protonmail.com>
2022-03-29 18:29:02 +0100

    OPS: Increase version number
    skipci

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("LICENSE") as f:
 
 setup(
     name="data_gateway",
-    version="0.11.5",
+    version="0.11.6",
     install_requires=[
         "click>=7.1.2",
         "pyserial==3.5",
