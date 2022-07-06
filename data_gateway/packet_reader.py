bdcd1af3acc887aa42eee5f700fc490a32e8f121
Tom Clark <tom@octue.com>
2022-07-06 11:22:33 +0000

    OPS: Prevent logger from flooding outputs in packet reader

--- data_gateway/packet_reader.py
+++ data_gateway/packet_reader.py
@@ -210,13 +210,13 @@ class PacketReader:
                                     "Received packet from node %s with unknown type: %s ", node_id, packet_type
                                 )
                                 continue
-                            else:
-                                logger.info(
-                                    "Processing packet from node %s with type %s (%s)",
-                                    node_id,
-                                    self.handles[node_id][packet_type],
-                                    packet_type,
-                                )
+                            # else:
+                            # logger.info(
+                            #     "Processing packet from node %s with type %s (%s)",
+                            #     node_id,
+                            #     self.handles[node_id][packet_type],
+                            #     packet_type,
+                            # )
 
                             if len(packet) == 244:  # If the full data payload is received, proceed parsing it
                                 timestamp = (
