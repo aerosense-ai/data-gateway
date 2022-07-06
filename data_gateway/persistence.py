bf125f998939e9e1a6a4a5ed063c82d25bbd742c
Tom Clark <tom@octue.com>
2022-07-06 12:09:50 +0000

    OPS: Add diagnostic to persistence

--- data_gateway/persistence.py
+++ data_gateway/persistence.py
@@ -257,7 +257,7 @@ class BatchingUploader(TimeBatcher):
         """
         try:
             logger.info("Uploading window to bucket_name %s", self.bucket_name)
-            logger.info("... with path %s", self._generate_window_path())
+            logger.info(".... with path %s", self._generate_window_path())
             self.client.upload_from_string(
                 string=json.dumps(self.ready_window),
                 cloud_path=storage.path.generate_gs_path(self.bucket_name, self._generate_window_path()),
